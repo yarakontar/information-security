ccda8eed555330f16794454c2a54456adb8df939
Yaraalkontar <yaraalqntar@gmail.com>
2023-10-19 16:29:46 +0300

    first homework

--- client.py
+++ client.py
@@ -1,15 +1,47 @@
-from cryptography.fernet import Fernet
+# from cryptography.fernet import Fernet
+# import socket
+
+# # العميل يستلم المفتاح المشترك من السيرفر
+# client_socket = socket.socket()
+# client_socket.connect(("127.0.0.10", 1345))
+
+# key = client_socket.recv(1024)
+# fernet = Fernet(key)
+
+# data = client_socket.recv(1024)
+# decrypted_message = fernet.decrypt(data)
+
+# print("Received:", decrypted_message.decode())
+# client_socket.close()
+
+
 import socket
+from Crypto.Cipher import AES
 
-# العميل يستلم المفتاح المشترك من السيرفر
-client_socket = socket.socket()
-client_socket.connect(("127.0.0.10", 1345))
+# المفتاح المشترك (يجب مشاركته مع الخادم بأمان)
+shared_key = b'ThisIsASharedKey'
 
-key = client_socket.recv(1024)
-fernet = Fernet(key)
+# إعداد العميل
+host = '127.0.0.1'
+port = 12346
 
-data = client_socket.recv(1024)
-decrypted_message = fernet.decrypt(data)
+# انشاء العميل
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((host, port))
 
-print("Received:", decrypted_message.decode())
-client_socket.close()
+    # استقبال الرسالة المشفرة من الخادم
+    received_data = client_socket.recv(1024)
+
+    # ال IV هو الجزء الأول من البيانات المستلمة
+    iv = received_data[:16]
+
+    # البيانات المشفرة بعد ال IV
+    ciphertext = received_data[16:]
+
+    # إعداد المشفر باستخدام المفتاح المشترك و IV
+    cipher = AES.new(shared_key, AES.MODE_CFB, iv)
+
+    # فك تشفير الرسالة
+    decrypted_message = cipher.decrypt(ciphertext).decode('utf-8')
+
+    print("yyy:", decrypted_message)
--- server.py
+++ server.py
@@ -1,23 +1,59 @@
-from cryptography.fernet import Fernet
+# from cryptography.fernet import Fernet
+# import socket
+
+
+# key = Fernet.generate_key()
+# fernet = Fernet(key)
+
+
+# server_socket = socket.socket()
+# server_socket.bind(("127.1.0.10", 1345))
+# server_socket.listen(5)
+# print("Server is listening...")
+
+# conn, addr = server_socket.accept()
+# print("Connection from:", addr)
+
+# conn.send(key)
+
+# message = "Hello, client!"
+# encrypted_message = fernet.encrypt(message.encode())
+# conn.send(encrypted_message)
+
+# conn.close()
 import socket
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
+# المفتاح المشترك (يجب مشاركته مع العميل بأمان)
+shared_key = b'ThisIsASharedKey'
 
-key = Fernet.generate_key()
-fernet = Fernet(key)
+# إعداد الخادم
+host = '127.0.0.1'
+port = 12346
 
+# إنشاء مولد عشوائي لل IV (Initialization Vector)
+iv = get_random_bytes(16)
 
-server_socket = socket.socket()
-server_socket.bind(("127.1.0.10", 1345))
-server_socket.listen(5)
-print("Server is listening...")
+# إعداد المشفر باستخدام مفتاح المشترك و IV
+cipher = AES.new(shared_key, AES.MODE_CFB, iv)
 
-conn, addr = server_socket.accept()
-print("Connection from:", addr)
+# انشاء السيرفر
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.bind((host, port))
+    server_socket.listen()
 
-conn.send(key)
+    print(f"hhhh {host}:{port}")
+    conn, addr = server_socket.accept()
 
-message = "Hello, client!"
-encrypted_message = fernet.encrypt(message.encode())
-conn.send(encrypted_message)
+    with conn:
+        print(f"tm{addr}")
 
-conn.close()
+        # الرسالة المراد إرسالها
+        message = "This is a secret message."
+
+        # تشفير الرسالة
+        ciphertext = iv + cipher.encrypt(message.encode('utf-8'))
+
+        # إرسال الرسالة المشفرة إلى العميل
+        conn.sendall(ciphertext)
